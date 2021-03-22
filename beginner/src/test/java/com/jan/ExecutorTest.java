d899cdde8aacc7239f74d250881f8b9cea059793
janvanrooyen <csr.spies@gmail.com>
2021-03-22 19:08:19 +0200

    Fixed sonarqube issues

--- beginner/src/test/java/com/jan/ExecutorTest.java
+++ beginner/src/test/java/com/jan/ExecutorTest.java
@@ -3,20 +3,18 @@ package com.jan;
 import com.jan.interfaces.IChallange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.hamcrest.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
-public class ExecutorTest {
+class ExecutorTest {
 
     IChallange codelandUserNameValidation;
 
     @BeforeEach
-    public void before() {
+    void before() {
         codelandUserNameValidation = mock(IChallange.class);
         when(codelandUserNameValidation.execute(any(Object.class))).thenReturn(new Object());
     }
