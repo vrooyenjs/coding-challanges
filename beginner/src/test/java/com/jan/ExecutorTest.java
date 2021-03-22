f5929ec632c064e0874cd80d68eaa431c00cadd0
janvanrooyen <csr.spies@gmail.com>
2021-03-22 21:14:05 +0200

    Added another challange

--- beginner/src/test/java/com/jan/ExecutorTest.java
+++ beginner/src/test/java/com/jan/ExecutorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.any;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
