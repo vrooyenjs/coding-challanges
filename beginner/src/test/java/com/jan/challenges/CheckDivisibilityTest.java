1af82c2fe45bb0df4a77e2e402dbaf23f0fa1a74
janvanrooyen <csr.spies@gmail.com>
2021-03-23 09:39:52 +0200

    Added another challange

--- beginner/src/test/java/com/jan/challenges/CheckDivisibilityTest.java
+++ beginner/src/test/java/com/jan/challenges/CheckDivisibilityTest.java
@@ -11,13 +11,13 @@ class CheckDivisibilityTest{
     IChallange checkDivisibility = new CheckDivisibility();
 
     @Test
-    public void testExecute_valid1(){
+    void testExecute_valid1(){
         assertTrue ((boolean)checkDivisibility.execute(new int[]{5, 5}));
         assertTrue ((boolean)checkDivisibility.execute(new int[]{10, 5}));
     }
 
     @Test
-    public void testExecute_valid2(){
+    void testExecute_valid2(){
         assertFalse ((boolean)checkDivisibility.execute(new int[]{33, 2}));
         assertFalse ((boolean)checkDivisibility.execute(new int[]{9, 8}));
         assertFalse ((boolean)checkDivisibility.execute(new int[]{0, 0}));
