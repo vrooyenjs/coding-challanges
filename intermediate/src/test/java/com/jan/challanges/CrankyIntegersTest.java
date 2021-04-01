8c7517aca0b8e00465439bcedd028fb6a6031086
vrooyenjs <csr.spies@gmail.com>
2021-04-01 10:23:15 +0200

    Gotta go faster -- Sanic

--- intermediate/src/test/java/com/jan/challanges/CrankyIntegersTest.java
+++ intermediate/src/test/java/com/jan/challanges/CrankyIntegersTest.java
@@ -2,6 +2,7 @@ package com.jan.challanges;
 
 import com.jan.interfaces.IChallange;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,49 +10,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Slf4j
 public class CrankyIntegersTest {
 
-    IChallange crankyIntegers;
+    private static CrankyIntegers crankyIntegers;
 
-    @Test
-    void testExecute_100() {
-        log.info("***************************************** Starting test");
+    @BeforeAll
+    static void beforeTests() {
         crankyIntegers = new CrankyIntegers();
-        assertEquals(0L, (long) crankyIntegers.execute(100L));
-
     }
 
     @Test
-    void testExecute_1000l() {
+    void testExecute_10_7_load() {
         log.info("***************************************** Starting test");
-        crankyIntegers = new CrankyIntegers();
+        long before = System.currentTimeMillis();
+        int iterations = 20;
+        long highNumber = 10000000L;
+        for (int i = 0 ; i < iterations ; i++){
+            assertEquals(5303474L, (long) crankyIntegers.execute(highNumber));
+        }
+        double timeTaken = (System.currentTimeMillis() - before);
+        float timePerCalc = (float) (timeTaken / (iterations * highNumber));
+        log.info("Completed iteration({}) of batch({}) in: {} -- ~(({} * {}) / {}) ms per calc",
+                iterations,
+                highNumber,
+                timeTaken,
+                iterations,
+                highNumber,
+                timeTaken);
+        // 1.0076E-4
+        // 0.00010076
+    }
+
+//    @Test
+    void testExecute_10_2() {
+        log.info("***************************************** Starting test");
+        assertEquals(0L, (long) crankyIntegers.execute(100L));
+    }
+
+//    @Test
+    void testExecute_10_3() {
+        log.info("***************************************** Starting test");
         assertEquals(1858L, (long) crankyIntegers.execute(1000L));
     }
 
-    @Test
-    void testExecute_10000l() {
+//    @Test
+    void testExecute_10_4() {
         log.info("***************************************** Starting test");
-        crankyIntegers = new CrankyIntegers();
         assertEquals(1858L, (long) crankyIntegers.execute(10000L));
     }
 
-    @Test
-    void testExecute_100000l() {
+//    @Test
+    void testExecute_10_5() {
         log.info("***************************************** Starting test");
-        crankyIntegers = new CrankyIntegers();
         assertEquals(1858L, (long) crankyIntegers.execute(100000L));
     }
 
-    @Test
-    void testExecute_1000000l() {
+//    @Test
+    void testExecute_10_6() {
         log.info("***************************************** Starting test");
-        crankyIntegers = new CrankyIntegers();
         assertEquals(1778723L, (long) crankyIntegers.execute(1000000L));
     }
 
-    @Test
-    void testExecute_10000000l() {
+//    @Test
+    void testExecute_10_7() {
         log.info("***************************************** Starting test");
-        crankyIntegers = new CrankyIntegers();
         assertEquals(5303474L, (long) crankyIntegers.execute(10000000L));
     }
 
+//    @Test
+    void testExecute_10_8() {
+        log.info("***************************************** Starting test");
+        assertEquals(139852339L, (long) crankyIntegers.execute(100000000L));
+    }
+
+
+//    @Test
+    void testExecute_10_9() {
+        log.info("***************************************** Starting test");
+        assertEquals(598515851L, (long) crankyIntegers.execute(1000000000L));
+    }
 }
